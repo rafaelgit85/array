9621ece76c8a77bfcdddfbb624982f3adebf9f84
rafaelgit85 <you@example.com>
2020-07-30 20:19:48 -0300

    Array

---
+++ prjCppArray/prjCppArray/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void main() {
+
+	short tabNumbers[3];
+
+	tabNumbers[0] = 28;
+	tabNumbers[1] = 3;
+	tabNumbers[2] = 2015;
+
+	/*cout << tabNumbers[0] << endl;
+	cout << tabNumbers[1] << endl;
+	cout << tabNumbers[2] << endl;*/
+
+
+	for (short i = 0; i < 3; i++) {
+		cout << tabNumbers[i] << endl;
+	}
+
+}
